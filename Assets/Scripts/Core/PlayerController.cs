e51b2d5eff57fe11df43355a4991d00c421c4f03
Patryk <Kopaczkapl@gmail.com>
2018-10-03 17:16:23 +0200

    Pending refactor

---
+++ Assets/Scripts/Core/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Core
+{
+	public class PlayerController : MonoBehaviour {
+	// Controls how the player moves, originally based on FPSWalker
+	
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	
+	private Vector3 moveDir = Vector3.zero;
+	private bool grounded = false;
+
+	void FixedUpdate()
+	{
+		if (grounded)
+		{
+			moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDir = Quaternion.AngleAxis(transform.localEulerAngles.y, Vector3.up) * moveDir;
+			//moveDir = transform.TransformDirection(moveDir);
+			moveDir *= speed;
+			
+			if (Input.GetButton("Jump"))
+			{
+				moveDir.y = jumpSpeed;
+			}
+		}
+		
+		moveDir.y -= gravity*Time.deltaTime;
+		
+		CharacterController controller = GetComponent<CharacterController>();
+		if(controller.enabled != false)
+		{
+			CollisionFlags flags = controller.Move(moveDir*Time.deltaTime);
+			grounded = (flags & CollisionFlags.CollidedBelow) != 0;
+		}
+	}
+}
+
+}
