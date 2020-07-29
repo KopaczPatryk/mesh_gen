b4b0e2cc9dd90763ffaa38753b9a4bd9fbd91c22
Patryk Kopacz <Kopaczkapl@gmail.com>
2020-07-29 22:05:40 +0200

    Legacy

--- Assets/Scripts/Core/ControlerOrbit.cs
+++ Assets/Scripts/Core/ControlerOrbit.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlerOrbit : MonoBehaviour {
-    
-	void Start() {
-		StartCoroutine(GoTo());
+
+	void Start () {
+		StartCoroutine (GoTo ());
 	}
-    
-	void Update() { }
-	public IEnumerator GoTo() {
+
+	void Update () { }
+
+	public IEnumerator GoTo () {
 		float progress = 0;
 		Vector3 startingPos = transform.position;
 		Vector3 pos = transform.position;
 		Vector3 target = Vector3.one * 5 - Random.insideUnitSphere * 15f;
 		while (true) {
 			progress += Time.deltaTime / 1;
-			transform.transform.position = Vector3.Lerp(pos, target, progress);
+			transform.transform.position = Vector3.Lerp (pos, target, progress);
 			if (progress >= 1) {
 				progress = 0;
 				pos = transform.position;
 				target = Vector3.one * 5 - Random.insideUnitSphere * 15f;
 			}
-			yield return new WaitForEndOfFrame();
+			yield return new WaitForEndOfFrame ();
 		}
 	}
 }
--- Assets/Scripts/MapGen/MainMapProvider.cs
+++ Assets/Scripts/MapGen/MainMapProvider.cs
@@ -4,41 +4,32 @@ using MeshGen.WorldGen;
 using UnityEngine;
 
 public class MainMapProvider : IMapProvider {
-    
+
 	private static IMapProvider instance;
 
-	public static IMapProvider GetInstance() {
-		instance = new MainMapProvider();
-		return instance;
+	public static IMapProvider GetInstance () {
+		return instance = new MainMapProvider ();
 	}
 
-    public Block GetBlock(Vector3 posAbs)
-    {
-        if (posAbs.y > 8)
-		{
-			return new MeshGen.WorldGen.Space();
+	public Block GetBlock (Vector3 posAbs) {
+		if (posAbs.y > 8) {
+			return new MeshGen.WorldGen.Space ();
+		} else {
+			return new Dirt ();
 		}
-		else
-        {
-			return new Dirt();
-        }
-    }
+	}
 
-    public Chunk GetChunk(Vector3 posAbs, int chunkSize)
-    {
-        Chunk chun = new Chunk(chunkSize);
+	public Chunk GetChunk (Vector3 posAbs, int chunkSize) {
+		Chunk chun = new Chunk (chunkSize);
 		int ObjectCount = 0;
 		for (int z = 0; z < chunkSize; z++) {
 			for (int y = 0; y < chunkSize; y++) {
 				for (int x = 0; x < chunkSize; x++) {
-                    //multiple smaller cubes in chunks
-                    if (x % 4 != 0 && y % 4 != 0 && z % 4 != 0)
-					{
-						chun.SetBlock(new Vector3Int(x,y,z), new Dirt());
-					}
-					else
-					{
-						chun.SetBlock(new Vector3Int(x,y,z), new MeshGen.WorldGen.Space());
+					//multiple smaller cubes in chunks
+					if (x % 4 != 0 && y % 4 != 0 && z % 4 != 0) {
+						chun.SetBlock (new Vector3Int (x, y, z), new Dirt ());
+					} else {
+						chun.SetBlock (new Vector3Int (x, y, z), new MeshGen.WorldGen.Space ());
 					}
 
 					// if (ObjectCount % 6 == 0)
@@ -56,6 +47,6 @@ public class MainMapProvider : IMapProvider {
 				}
 			}
 		}
-        return chun;
-    }
+		return chun;
+	}
 }
